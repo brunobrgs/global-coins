45769ec9d9668bdf8835cd8c7d5863feaeb96345
Bruno Borges <bruno.rails@gmail.com>
2018-12-06 15:49:31 +0000

    Fix route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   root "status#index"
   resources :users, only: [:show]
   resources :transactions, only: [:index, :show, :create]
-  get 'payment_response', to: 'transactions#payment_response', as: :payment_response
-  post 'payment_response', to: 'transactions#payment_response', as: :payment_response
+  match "transactions/payment_response" => "transactions#payment_response", as: :payment_response, via: [:get, :post]
 end
