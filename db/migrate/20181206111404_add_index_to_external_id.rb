4de9ad3564de90878bd72dfb88174f9da46e4fd1
Bruno Borges <bruno.rails@gmail.com>
2018-12-06 11:15:02 +0000

    Add index to user external_id

---
+++ db/migrate/20181206111404_add_index_to_external_id.rb
@@ -0,0 +1,5 @@
+class AddIndexToExternalId < ActiveRecord::Migration[5.2]
+  def change
+    add_index :users, :external_id, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_06_110757) do
+ActiveRecord::Schema.define(version: 2018_12_06_111404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2018_12_06_110757) do
     t.integer "balance", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["external_id"], name: "index_users_on_external_id", unique: true
   end
 
 end
